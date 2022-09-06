95a3d706e3ddd13380302136c46594fd2d22e085
Matheus Phelipe <matheusphalves@gmail.com>
2022-09-05 22:00:38 -0300

    testing

--- algorithms_ALP/main.py
+++ algorithms_ALP/main.py
@@ -6,17 +6,20 @@ from algorithms_ALP.src.utils.handlers.FileHandler import FileHandler
 
 if __name__ == '__main__':
     # df = DataFrameHandler.read_csv_data('C:\\Users\\mathe\\Desktop\\workspace\\algorithms-aircraft-landing-problems\\algorithms_ALP\\tmp\\airland_1662239820256920500.csv')
-    df = DataFrameHandler.read_csv_data('C:\\Users\\mathe\\Desktop\\workspace\\algorithms-aircraft-landing-problems\\algorithms_ALP\\tmp\\airland8_1662311765763438000.csv')
+    df = DataFrameHandler.read_csv_data('C:\\Users\\mathe\\Desktop\\workspace\\algorithms-aircraft-landing-problems\\algorithms_ALP\\tmp\\airland_1662239820256920500.csv')
     alp = ALPInstance(df)
     alp.build_ALP_instance()
 
     aco_solver = ACOSolver(
-        runaway_number = 1,
-        number_of_ants = 100,
-        evaporation_rate = 0.2,
+        runaway_number = 2,
+        number_of_ants = 500,
+        evaporation_rate = 0.8,
         pheromone_intensity = 1,
+        beta=0.3,
+        beta1=0.9,
+        beta2=0.9,
         beta_evaporation_rate = 1)
-    aco_solver.start(alp_intance=alp)
+    aco_solver.start(alp_intance=alp, max_iterations=200)
     x = 0
 
     # alp_parser = ALPParser()
